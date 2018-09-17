546284dea259f916d9d6a8d2592a3790c93dfa0c
frtoud <francois.toulouse@polymtl.ca>
2018-09-17 13:33:11 -0400

    Init repo with multi jump

---
+++ Cadriciel_2018_v5/Assets/Sample Assets/Effects/Particle Systems/scripts/ExtinguishableParticleSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtinguishableParticleSystem : MonoBehaviour {
+
+	public float multiplier = 1;
+	ParticleSystem[] systems;
+
+	void Start()
+	{
+		systems = GetComponentsInChildren<ParticleSystem>();
+	}
+
+	public void Extinguish()
+	{
+		foreach (var system in systems) {
+			var emission = system.emission;
+            emission.enabled = false;
+		}
+	}
+}
