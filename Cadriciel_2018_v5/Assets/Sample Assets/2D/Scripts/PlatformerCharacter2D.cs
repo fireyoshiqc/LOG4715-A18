9d3390b0a1624b8db80f072d985c33c891fbb6a6
frtoud <francois.toulouse@polymtl.ca>
2018-09-17 15:41:55 -0400

    [TP1] Walljumps implemented

--- Cadriciel_2018_v5/Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Cadriciel_2018_v5/Assets/Sample Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -4,17 +4,21 @@ public class PlatformerCharacter2D : MonoBehaviour
 {
 	bool facingRight = true;							// For determining which way the player is currently facing.
 
+    [SerializeField] float HorizontalAcceleration = 1f;
 	[SerializeField] float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
 	[SerializeField] float jumpForce = 400f;			// Amount of force added when the player jumps.	
+	[SerializeField] float wallJumpMult = 1f;			// Relative power of a Wall Jump.	
+	[SerializeField] float WallHorizontalForce = 40000f;	// Amount of force added when the player walljump (horizontal).	
 
 	[Range(0, 1)]
 	[SerializeField] float crouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	
 	[SerializeField] bool airControl = false;			// Whether or not a player can steer while jumping;
 	[SerializeField] LayerMask whatIsGround;			// A mask determining what is ground to the character
+	[SerializeField] LayerMask whatIsWall;  			// A mask determining what is walls to the character
 	
 	Transform groundCheck;								// A position marking where to check if the player is grounded.
-	float groundedRadius = .2f;							// Radius of the overlap circle to determine if grounded
+	float groundedRadius = .12f;							// Radius of the overlap circle to determine if grounded
 	bool grounded = false;								// Whether or not the player is grounded.
 	Transform ceilingCheck;								// A position marking where to check for ceilings
 	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
@@ -24,12 +28,22 @@ public class PlatformerCharacter2D : MonoBehaviour
     [SerializeField] int maxAirJumps = 1;
     private int AirJumpCounter = 0;
 
+    [SerializeField] bool wallJumpResetsAirJumps = false;
+    Transform wallCheckRight;
+    Transform wallCheckLeft;
+    float walledRadius = 0.2f;
+    bool walledRight = false;
+    bool walledLeft = false;
+    
+
     void Awake()
 	{
 		// Setting up references.
 		groundCheck = transform.Find("GroundCheck");
 		ceilingCheck = transform.Find("CeilingCheck");
-		anim = GetComponent<Animator>();
+        wallCheckRight = transform.Find("WallCheckRight");
+        wallCheckLeft = transform.Find("WallCheckLeft");
+        anim = GetComponent<Animator>();
 	}
 
 
@@ -38,9 +52,12 @@ public class PlatformerCharacter2D : MonoBehaviour
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
 		anim.SetBool("Ground", grounded);
+        
+        walledRight = Physics2D.OverlapCircle(wallCheckRight.position, walledRadius, whatIsWall);
+        walledLeft = Physics2D.OverlapCircle(wallCheckLeft.position, walledRadius, whatIsWall);
 
-		// Set the vertical animation
-		anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+        // Set the vertical animation
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
 	}
 
 
@@ -62,14 +79,19 @@ public class PlatformerCharacter2D : MonoBehaviour
 		//only control the player if grounded or airControl is turned on
 		if(grounded || airControl)
 		{
-			// Reduce the speed if crouching by the crouchSpeed multiplier
-			move = (crouch ? move * crouchSpeed : move);
 
-			// The Speed animator parameter is set to the absolute value of the horizontal input.
-			anim.SetFloat("Speed", Mathf.Abs(move));
+            // Reduce the speed if crouching by the crouchSpeed multiplier
+            move = (crouch ? move * crouchSpeed : move);
 
+            float slowing = grounded ? 0.35f : 0.95f;
+            float xSpeed = GetComponent<Rigidbody2D>().velocity.x * slowing;
+
+            xSpeed += move * HorizontalAcceleration;
+
+            xSpeed = Mathf.Clamp(xSpeed, -maxSpeed, +maxSpeed);
 			// Move the character
-			GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
 			
 			// If the input is moving the player right and the player is facing left...
 			if(move > 0 && !facingRight)
@@ -79,6 +101,9 @@ public class PlatformerCharacter2D : MonoBehaviour
 			else if(move < 0 && facingRight)
 				// ... flip the player.
 				Flip();
+
+			// The Speed animator parameter is set to the absolute value of the horizontal input.
+			anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
 		}
 
         if(grounded)
@@ -87,13 +112,27 @@ public class PlatformerCharacter2D : MonoBehaviour
             AirJumpCounter = maxAirJumps;
         }
 
+        if ( !grounded && (walledRight || walledLeft) && jump)
+        {
+            //We have contacc on a wall and want to jump
+
+            //NOBODY EVER TOUCH THIS
+            float flippedForce = walledLeft ? 1 : -1;
+            float flippedAgain = facingRight ? 1 : -1;
+
+            Jump(flippedForce * flippedAgain * WallHorizontalForce, jumpForce * wallJumpMult);
+            if (wallJumpResetsAirJumps)
+            {
+                AirJumpCounter = maxAirJumps;
+            }
+        }
+
         // If the player should jump...
         // Player must have remaining jumps
-        if ((grounded || AirJumpCounter > 0) && jump) {
+        if ((grounded || !(walledRight || walledLeft) &&  AirJumpCounter > 0) && jump) {
             // Add a vertical force to the player.
             anim.SetBool("Ground", false);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+            Jump(0f, jumpForce);
 
             //Consume a jump
             if (!grounded)
@@ -103,6 +142,11 @@ public class PlatformerCharacter2D : MonoBehaviour
         }
 	}
 
+    void Jump(float x, float y)
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(x, y));
+    }
 	
 	void Flip ()
 	{
