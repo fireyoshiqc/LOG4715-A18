4373ded9220c1cfeca76519b4700b5c422bba3bd
Fudgyking <escalado19@hotmail.com>
2018-10-31 01:15:31 -0400

    Brasiers qui s'allument lorsqu'une torche passe dans une certaine zone

---
+++ TP2/Gym6_Braisier/Assets/BrasierAction.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrasierAction : MonoBehaviour {
+
+    public GameObject torch;
+    public bool lit = false; 
+
+	// Use this for initialization
+	void Start () {
+        PutOut();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (lit) return;
+        if (col.gameObject.tag == "torch")
+        {
+            if (!torch)
+                torch = col.gameObject;
+
+            lit = true;
+            LightItUp();
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        
+    }
+
+    void LightItUp()
+    {
+        GetComponentInChildren<ParticleSystem>().Play();
+        Light[] lights = GetComponentsInChildren<Light>();
+        foreach (Light light in lights)
+        {
+            light.enabled = true;
+        }
+    }
+
+    void PutOut()
+    {
+        GetComponentInChildren<ParticleSystem>().Stop();
+        Light[] lights = GetComponentsInChildren<Light>();
+        foreach (Light light in lights)
+        {
+            light.enabled = false;
+        }
+    }
+}
---
+++ TP2/Gym6_Braisier/Assets/FireController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour {
+    
+    float currentTime = 0;
+    
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime = currentTime + Time.deltaTime;
+    }
+
+    public void Lit()
+    {
+        GetComponent<Renderer>().enabled = true;
+        Light[] lights = GetComponentsInChildren<Light>();
+        foreach (Light light in lights)
+        {
+            light.enabled = true;
+        }
+    }
+}
