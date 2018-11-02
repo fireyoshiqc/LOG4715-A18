03c3cc686d82930bb0f0bc7152357bfbe66f60d9
audrey <audrey.lebelr@gmail.com>
2018-11-01 22:55:38 -0400

    Fixed rotation choppiness issue and cleaned script

--- TP2/Gym3_BalanciersPoulies/Assets/Scripts/BalancingPlatform.cs
+++ TP2/Gym3_BalanciersPoulies/Assets/Scripts/BalancingPlatform.cs
@@ -5,74 +5,35 @@ using System.Collections.Generic;
 public class BalancingPlatform : MonoBehaviour {
 
     [SerializeField]
-    float minimumX = -40;
+    float mimimumAngle = -40;
     [SerializeField]
-    float maximumX = 40;
+    float maximumAngle = 40;
     [SerializeField]
-    float speed = 1f;
-    float rotationX = 0F;
-    private List<float> rotArrayX = new List<float>();
-	float rotAverageX = 0F;	
-    public float frameCounter = 20;
+    float speed = 50f;
 
+	float rotationToApply = 0f;	
+    float currentRotation = 0f;
     bool isClockwise, isMoving = false;
-    float startTime;
-    float delayToStop = 5f;
-    Quaternion clockWise, counterClockWise;
-    Quaternion originalRotation;
 
     void Start () {
-        //clockWise = new Quaternion(maxRotation, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-        //counterClockWise = new Quaternion(-maxRotation, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-        originalRotation = transform.localRotation;
-        //length = Quaternion.Angle(clockWise, counterClockWise);
     }
 
     void Update()
     {
-        if (Time.time - startTime > delayToStop)
-        {
-            isMoving = false;
-        }
 
         if (isMoving)
         {
-            rotAverageX = 0f;
-		    rotationX = (isClockwise)? rotationX - speed : rotationX + speed;
-		    rotArrayX.Add(rotationX);
- 
-		    if (rotArrayX.Count >= frameCounter) {
-			    rotArrayX.RemoveAt(0);
-		    }
-		    for(int i = 0; i < rotArrayX.Count; i++) {
-			    rotAverageX += rotArrayX[i];
-		    }
-		    rotAverageX /= rotArrayX.Count;
- 
-		    rotAverageX = ClampAngle(rotationX, minimumX, maximumX);
- 
-		    Quaternion xQuaternion = Quaternion.AngleAxis (rotAverageX, Vector3.left);
-		    //transform.localRotation = originalRotation * xQuaternion;	
-            //transform.Rotate(Vector3.left, rotAverageX);
-            transform.rotation = Quaternion.Lerp(transform.rotation, originalRotation * xQuaternion, speed);
-        }
-        
-        //if (isClockwise && isMoving)
-        //{
-        //    //transform.rotation = Quaternion.Lerp(transform.rotation, clockWise, speed);
+		    rotationToApply = (isClockwise)? - (speed * Time.deltaTime) : + (speed * Time.deltaTime); 
+            currentRotation += rotationToApply;
 
-        //    transform.Rotate(Vector3.left * Time.deltaTime * speed);
-        //}
-        //else if (isMoving)
-        //{
-        //    //transform.rotation = Quaternion.Lerp(transform.rotation, counterClockWise, speed);
-        //    Vector3 rotationToApply = Vector3.right * Time.deltaTime * speed;
-        //    transform.rotation.
-        //    if (Mathf.Abs((rotationToApply.x + transform.rotation.x)) < maxRotation)
-        //    {
-        //        transform.Rotate(rotationToApply);
-        //    }
-        //}
+            if (currentRotation > mimimumAngle && currentRotation  < maximumAngle)
+            {
+                transform.Rotate(rotationToApply, 0, 0);
+            } else
+            {
+                currentRotation -= rotationToApply;
+            }
+        }
     }
 
     void OnCollisionEnter(Collision col)
@@ -82,27 +43,11 @@ public class BalancingPlatform : MonoBehaviour {
 
     private void OnCollisionStay(Collision collision)
     {
-        startTime = Time.time;
         isClockwise = (transform.position.z - collision.transform.position.z > 0);
     }
 
-    //void OnCollisionExit(Collision col)
-    //{
-    //    this.startTime = Time.time;
-    //    //isMoving = false;
-    //}
-
-    float ClampAngle (float angle, float min, float max)
-	{
-		angle = angle % 360;
-		if ((angle >= -360F) && (angle <= 360F)) {
-			if (angle < -360F) {
-				angle += 360F;
-			}
-			if (angle > 360F) {
-				angle -= 360F;
-			}			
-		}
-		return Mathf.Clamp (angle, min, max);
-	}
+    void OnCollisionExit(Collision col)
+    {
+        isMoving = false;
+    }
 }
