6b3a5570351e2c39c16d2cc1d4e626fae6a67761
Félix Boulet <fire_yoshi_qc@hotmail.com>
2018-11-04 01:28:15 -0400

    Finished Gym 5 with waterfall.

---
+++ TP2/Gym1_Lancer/Assets/WaterfallController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterfallController : MonoBehaviour {
+
+  public float flameReductionStrength = 1.0f;
+  private BasicTorchController torch;
+
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (torch && torch.flameLife > 0.0f)
+    {
+      torch.flameLife -= flameReductionStrength * Time.deltaTime;
+    }
+	}
+
+  private void OnTriggerEnter(Collider col)
+  {
+    if (col.gameObject.tag == "torch")
+    {
+      if (!torch)
+        torch = col.gameObject.GetComponent<BasicTorchController>();
+    }
+  }
+
+  private void OnTriggerStay(Collider col)
+  {
+    if (col.gameObject.tag == "torch")
+    {
+      if (!torch)
+        torch = col.gameObject.GetComponent<BasicTorchController>();
+    }
+  }
+
+  private void OnTriggerExit(Collider col)
+  {
+    torch = null;
+  }
+}
