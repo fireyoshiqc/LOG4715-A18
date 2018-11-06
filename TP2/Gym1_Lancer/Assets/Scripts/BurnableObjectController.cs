02ee816c5028b5df9caf4947db35747041969793
Fudgyking <escalado19@hotmail.com>
2018-11-05 21:29:54 -0500

    Fireballs mostly working

--- TP2/Gym1_Lancer/Assets/Scripts/BurnableObjectController.cs
+++ TP2/Gym1_Lancer/Assets/Scripts/BurnableObjectController.cs
@@ -5,191 +5,181 @@ using UnityEngine;
 public class BurnableObjectController : MonoBehaviour
 {
 
-  public bool lit = false;
-  public float burnTime = 10.0f;
-  public int lightSmoothingFactor = 10; // Smoothness of light flickering
-  public float lightIntensity = 5.0f;
-  private Color startColor;
-  private Color delta;
-  private Material burnShader;
-  private float initialBurnTime;
-  private FlameController torch;
-  private ParticleSystem.EmissionModule flameEmission;
-  private ParticleSystem.EmissionModule ashEmission;
-  private Light[] lights;
-  private float[] lightSmoothing;
+    public bool lit = false;
+    public float burnTime = 10.0f;
+    public int lightSmoothingFactor = 10; // Smoothness of light flickering
+    public float lightIntensity = 5.0f;
+    private Color startColor;
+    private Color delta;
+    private Material burnShader;
+    private float initialBurnTime;
+    private FlameController torch;
+    private ParticleSystem.EmissionModule flameEmission;
+    private ParticleSystem.EmissionModule ashEmission;
+    private Light[] lights;
+    private float[] lightSmoothing;
 
-  // Use this for initialization
-  void Start()
-  {
-    startColor = GetComponent<Renderer>().materials[0].color;
-    initialBurnTime = burnTime;
-    delta = startColor / burnTime;
-    if (GetComponent<Renderer>().materials.Length > 1)
+    // Use this for initialization
+    void Start()
     {
-      burnShader = GetComponent<Renderer>().materials[1];
-      burnShader.SetFloat("_DissolveAmount", 1.0f);
+        startColor = GetComponent<Renderer>().materials[0].color;
+        initialBurnTime = burnTime;
+        delta = startColor / burnTime;
+        if (GetComponent<Renderer>().materials.Length > 1)
+        {
+            burnShader = GetComponent<Renderer>().materials[1];
+            burnShader.SetFloat("_DissolveAmount", 1.0f);
+        }
+        flameEmission = GetComponentsInChildren<ParticleSystem>()[0].emission;
+        flameEmission.enabled = false;
+        ashEmission = GetComponentsInChildren<ParticleSystem>()[1].emission;
+        ashEmission.enabled = false;
+        lights = GetComponentsInChildren<Light>();
+        foreach (Light light in lights)
+        {
+            light.enabled = false;
+        }
+        lightSmoothing = new float[lightSmoothingFactor];
+        for (int i = 0; i < lightSmoothing.Length; i++)
+        {
+            lightSmoothing[i] = 0.0f;
+        }
     }
-    flameEmission = GetComponentsInChildren<ParticleSystem>()[0].emission;
-    flameEmission.enabled = false;
-    ashEmission = GetComponentsInChildren<ParticleSystem>()[1].emission;
-    ashEmission.enabled = false;
-    lights = GetComponentsInChildren<Light>();
-    foreach (Light light in lights)
-    {
-      light.enabled = false;
-    }
-    lightSmoothing = new float[lightSmoothingFactor];
-    for (int i = 0; i < lightSmoothing.Length; i++)
-    {
-      lightSmoothing[i] = 0.0f;
-    }
-  }
 
-  // Update is called once per frame
-  void Update()
-  {
-    if (lit && burnTime > 0.0f)
+    // Update is called once per frame
+    void Update()
     {
-      burnTime -= Time.deltaTime;
-      GetComponent<Renderer>().materials[0].color -= (delta * Time.deltaTime);
-      if (burnShader != null)
-        burnShader.SetFloat("_DissolveAmount", (burnTime / initialBurnTime));
-      flameEmission.enabled = true;
-      UpdateLights();
+        if (lit && burnTime > 0.0f)
+        {
+            burnTime -= Time.deltaTime;
+            GetComponent<Renderer>().materials[0].color -= (delta * Time.deltaTime);
+            if (burnShader != null)
+                burnShader.SetFloat("_DissolveAmount", (burnTime / initialBurnTime));
+            flameEmission.enabled = true;
+            UpdateLights();
+        }
+        else
+        {
+            flameEmission.enabled = false;
+            ashEmission.enabled = false;
+            foreach (Light light in lights)
+            {
+                light.enabled = false;
+            }
+            if (lit)
+            {
+                GetComponent<Renderer>().enabled = false;
+                foreach (Collider collider in GetComponents<Collider>())
+                {
+                    collider.enabled = false;
+                }
+                ashEmission.enabled = true;
+                GetComponentsInChildren<ParticleSystem>()[1].Play();
+                Destroy(gameObject, 2.0f);
+                this.enabled = false;
+            }
+        }
     }
-    else
+
+    private void OnTriggerEnter(Collider col)
     {
-      flameEmission.enabled = false;
-      ashEmission.enabled = false;
+        HandleTrigger(col);
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        HandleTrigger(col);
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "torch" || col.gameObject.tag == "lantern")
+        {
+            if (torch)
+                torch = null;
+        }
+    }
+
+    private void HandleTrigger(Collider col)
+    {
+        if (col.gameObject.tag == "torch" || col.gameObject.tag == "lantern" )
+        {
+            if (!torch || col.gameObject.GetComponent<FlameController>().flameLife > torch.flameLife)
+                torch = col.gameObject.GetComponent<FlameController>();
+            if (lit)
+            {
+                torch.flameLife = torch.maxFlameLife;
+            }
+            else if (torch.flameLife > 0.0f)
+            {
+                lit = true;
+                flameEmission.enabled = true;
+            }
+        }
+        else if (col.gameObject.tag == "burns")
+        {
+            if (lit)
+                col.gameObject.GetComponent<BurnableObjectController>().lit = true;
+        }
+        else if(col.gameObject.tag == "fireball") {
+            lit = true;
+            flameEmission.enabled = true;
+        }
+    }
+    private void UpdateLights()
+    {
+        // https://answers.unity.com/questions/34739/how-to-make-a-light-flicker.html
+        float sum = .0f;
+
+        // Shift values in the table so that the new one is at the
+        // end and the older one is deleted.
+        for (int i = 1; i < lightSmoothing.Length; i++)
+        {
+            lightSmoothing[i - 1] = lightSmoothing[i];
+            sum += lightSmoothing[i - 1];
+        }
+
+        // Add the new value at the end of the array.
+        float burnLight = lightIntensity * (burnTime / initialBurnTime);
+        lightSmoothing[lightSmoothing.Length - 1] = Random.Range(burnLight * 0.9f, burnLight * 1.1f);
+        sum += lightSmoothing[lightSmoothing.Length - 1];
+
+        // Compute the average of the array and assign it to the
+        // light intensity.
+        foreach (Light light in lights)
+        {
+            light.enabled = true;
+            light.intensity = sum / lightSmoothing.Length;
+        }
+    }
+
+    private void CreateAshes()
+    {
+    }
+
+    /*
+    void LightItUp()
+    {
+      GetComponentInChildren<ParticleSystem>().Play();
+      Light[] lights = GetComponentsInChildren<Light>();
+      foreach (Light light in lights)
+      {
+        light.enabled = true;
+      }
+      player.SpawnPos.y = transform.position.y;
+      player.SpawnPos.z = transform.position.z;
+      if (LinkedDoor)
+        LinkedDoor.InteractedUpdate(true);
+    }
+
+    void PutOut()
+    {
+      GetComponentInChildren<ParticleSystem>().Stop();
+      Light[] lights = GetComponentsInChildren<Light>();
       foreach (Light light in lights)
       {
         light.enabled = false;
       }
-      if (lit)
-      {
-        GetComponent<Renderer>().enabled = false;
-        foreach (Collider collider in GetComponents<Collider>())
-        {
-          collider.enabled = false;
-        }
-        ashEmission.enabled = true;
-        GetComponentsInChildren<ParticleSystem>()[1].Play();
-        Destroy(gameObject, 2.0f);
-        this.enabled = false;
-      }
     }
-  }
-
-  private void OnTriggerEnter(Collider col)
-  {
-    if (col.gameObject.tag == "torch" || col.gameObject.tag == "lantern")
-    {
-      if (!torch || col.gameObject.GetComponent<FlameController>().flameLife > torch.flameLife)
-        torch = col.gameObject.GetComponent<FlameController>();
-      if (lit)
-      {
-        torch.flameLife = torch.maxFlameLife;
-      }
-      else if (torch.flameLife > 0.0f)
-      {
-        lit = true;
-        flameEmission.enabled = true;
-      }
-    }
-    else if (col.gameObject.tag == "burns")
-    {
-      if (lit)
-        col.gameObject.GetComponent<BurnableObjectController>().lit = true;
-    }
-  }
-
-  private void OnTriggerStay(Collider col)
-  {
-    if (col.gameObject.tag == "torch" || col.gameObject.tag == "lantern")
-    {
-      if (!torch || col.gameObject.GetComponent<FlameController>().flameLife > torch.flameLife)
-        torch = col.gameObject.GetComponent<FlameController>();
-      if (lit)
-      {
-        torch.flameLife = torch.maxFlameLife;
-      }
-      else if (torch.flameLife > 0.0f)
-      {
-        lit = true;
-        flameEmission.enabled = true;
-      }
-    }
-    else if (col.gameObject.tag == "burns")
-    {
-      if (lit)
-        col.gameObject.GetComponent<BurnableObjectController>().lit = true;
-    }
-  }
-
-  private void OnTriggerExit(Collider col)
-  {
-    if (col.gameObject.tag == "torch" || col.gameObject.tag == "lantern")
-    {
-      if (torch)
-        torch = null;
-    }
-  }
-
-  private void UpdateLights()
-  {
-    // https://answers.unity.com/questions/34739/how-to-make-a-light-flicker.html
-    float sum = .0f;
-
-    // Shift values in the table so that the new one is at the
-    // end and the older one is deleted.
-    for (int i = 1; i < lightSmoothing.Length; i++)
-    {
-      lightSmoothing[i - 1] = lightSmoothing[i];
-      sum += lightSmoothing[i - 1];
-    }
-
-    // Add the new value at the end of the array.
-    float burnLight = lightIntensity * (burnTime / initialBurnTime);
-    lightSmoothing[lightSmoothing.Length - 1] = Random.Range(burnLight * 0.9f, burnLight * 1.1f);
-    sum += lightSmoothing[lightSmoothing.Length - 1];
-
-    // Compute the average of the array and assign it to the
-    // light intensity.
-    foreach (Light light in lights)
-    {
-      light.enabled = true;
-      light.intensity = sum / lightSmoothing.Length;
-    }
-  }
-
-  private void CreateAshes()
-  {
-  }
-
-  /*
-  void LightItUp()
-  {
-    GetComponentInChildren<ParticleSystem>().Play();
-    Light[] lights = GetComponentsInChildren<Light>();
-    foreach (Light light in lights)
-    {
-      light.enabled = true;
-    }
-    player.SpawnPos.y = transform.position.y;
-    player.SpawnPos.z = transform.position.z;
-    if (LinkedDoor)
-      LinkedDoor.InteractedUpdate(true);
-  }
-
-  void PutOut()
-  {
-    GetComponentInChildren<ParticleSystem>().Stop();
-    Light[] lights = GetComponentsInChildren<Light>();
-    foreach (Light light in lights)
-    {
-      light.enabled = false;
-    }
-  }
-  */
+    */
 }
--- TP2/Gym1_Lancer/Assets/Scripts/TorchActions.cs
+++ TP2/Gym1_Lancer/Assets/Scripts/TorchActions.cs
@@ -5,267 +5,268 @@ using UnityEngine;
 public class TorchActions : MonoBehaviour
 {
 
-  public GameObject torch;
-  public GameObject lantern;
-  public GameObject scepter;
-  public GameObject currentlyHeld;
-  public Transform guide;
-  public Transform brick;
+    public GameObject torch;
+    public GameObject lantern;
+    public GameObject scepter;
+    public GameObject currentlyHeld;
+    public Transform guide;
+    public Transform fireball;
 
-  public float maxThrowForce = 500.0f;
-  public float throwChargeRate = 350.0f;
-  private float _currentThrowForce = 0.0f;
+    public float maxThrowForce = 500.0f;
+    public float throwChargeRate = 350.0f;
+    public float fireBallBaseSpeed = 3;
+    private float _currentThrowForce = 0.0f;
 
-  public float targetLineLengthModifier = 3.0f;
-  public float targetLineMaxWidth = 0.05f;
+    public float targetLineLengthModifier = 3.0f;
+    public float targetLineMaxWidth = 0.05f;
 
-  private LineRenderer _lineRenderer;
+    private LineRenderer _lineRenderer;
 
-  // Use this for initialization
-  void Start()
-  {
-    _lineRenderer = gameObject.AddComponent<LineRenderer>();
-    _lineRenderer.startWidth = targetLineMaxWidth;
-    _lineRenderer.endWidth = targetLineMaxWidth;
-    _lineRenderer.enabled = false;
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-    if (Input.GetKeyDown(KeyCode.E))
+    // Use this for initialization
+    void Start()
     {
-      if (currentlyHeld)
-        Drop();
-      else
-        Pickup();
+        _lineRenderer = gameObject.AddComponent<LineRenderer>();
+        _lineRenderer.startWidth = targetLineMaxWidth;
+        _lineRenderer.endWidth = targetLineMaxWidth;
+        _lineRenderer.enabled = false;
     }
 
-    if (currentlyHeld)
+    // Update is called once per frame
+    void Update()
     {
-      if (currentlyHeld != scepter)
-        currentlyHeld.transform.position = guide.position;
-      else
-        scepter.transform.position = guide.position + new Vector3(0f, 1f, 0f);
-    }
 
-
-    if (Input.GetMouseButton(0))
-    {
-      if (currentlyHeld)
-      {
-        DrawThrowTarget();
-        if (_currentThrowForce < maxThrowForce)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-          _currentThrowForce += throwChargeRate * Time.deltaTime; // Multiply by deltatime to ensure it's framerate-independant
-          if (_currentThrowForce >= maxThrowForce)
-            _currentThrowForce = maxThrowForce;
+            if (currentlyHeld)
+                Drop();
+            else
+                Pickup();
         }
-      }
-      else
-      {
+
+        if (currentlyHeld)
+        {
+            if (currentlyHeld != scepter)
+                currentlyHeld.transform.position = guide.position;
+            else
+                scepter.transform.position = guide.position + new Vector3(0f, 1f, 0f);
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            if (currentlyHeld)
+            {
+                DrawThrowTarget();
+                if (_currentThrowForce < maxThrowForce)
+                {
+                    _currentThrowForce += throwChargeRate * Time.deltaTime; // Multiply by deltatime to ensure it's framerate-independant
+                    if (_currentThrowForce >= maxThrowForce)
+                        _currentThrowForce = maxThrowForce;
+                }
+            }
+            else
+            {
+                _currentThrowForce = 0.0f;
+                _lineRenderer.enabled = false;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (currentlyHeld != scepter)
+            {
+                Throw();
+            }
+            else
+            {
+                ShootFireBall();
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "torch")
+        {
+            if (!torch)
+                torch = col.gameObject;
+        }
+        if (col.gameObject.tag == "lantern")
+        {
+            if (!lantern)
+                lantern = col.gameObject;
+        }
+        if (col.gameObject.tag == "scepter")
+        {
+            if (!scepter)
+                scepter = col.gameObject;
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "torch")
+        {
+            if (!torch)
+                torch = col.gameObject;
+        }
+        if (col.gameObject.tag == "lantern")
+        {
+            if (!lantern)
+                lantern = col.gameObject;
+        }
+        if (col.gameObject.tag == "scepter")
+        {
+            if (!scepter)
+                scepter = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "torch")
+        {
+            if (torch)
+                torch = null;
+        }
+        if (col.gameObject.tag == "lantern")
+        {
+            if (lantern)
+                lantern = null;
+        }
+        if (col.gameObject.tag == "scepter")
+        {
+            if (scepter)
+                scepter = null;
+        }
+    }
+
+    private void Pickup()
+    {
+        if (!(torch || lantern || scepter))
+            return;
+        if (torch)
+        {
+            torch.GetComponent<Rigidbody>().isKinematic = true;
+            torch.transform.SetParent(guide);
+            torch.transform.localRotation = guide.rotation;
+            torch.transform.Rotate(0, 0, 180); // Rotate otherwise it's upside-down...
+            torch.transform.position = guide.position;
+            currentlyHeld = torch;
+        }
+        else if (lantern)
+        {
+            lantern.GetComponent<Rigidbody>().isKinematic = true;
+            lantern.transform.SetParent(guide);
+            lantern.transform.localRotation = guide.rotation;
+            lantern.transform.Rotate(-90, 0, 0); // Rotate for 3D model
+            lantern.transform.position = guide.position;
+            currentlyHeld = lantern;
+        }
+        else if (scepter)
+        {
+            scepter.GetComponent<Rigidbody>().isKinematic = true;
+            scepter.transform.SetParent(guide);
+            scepter.transform.localRotation = guide.rotation;
+            scepter.transform.Rotate(-90, 0, 0); // Rotate for 3D model
+            scepter.transform.position = guide.position + new Vector3(0f, 1f, 0f);
+            currentlyHeld = scepter;
+        }
+    }
+
+    private void Drop()
+    {
+        if (!currentlyHeld)
+            return;
+        if (currentlyHeld == torch)
+        {
+            torch = null;
+        }
+        if (currentlyHeld == lantern)
+        {
+            lantern = null;
+        }
+        if (currentlyHeld == scepter)
+        {
+            scepter = null;
+        }
+        currentlyHeld.GetComponent<Rigidbody>().isKinematic = false;
+        guide.GetChild(0).parent = null;
+        currentlyHeld = null;
+        if (torch || lantern || scepter)
+        {
+            Pickup();
+        }
+    }
+
+    private void DrawThrowTarget()
+    {
+        if (!currentlyHeld)
+            return;
+
+        Vector3 mousePos = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            Vector3 direction = (hit.point - currentlyHeld.transform.position).normalized;
+
+            float forceRatio = (_currentThrowForce / maxThrowForce);
+
+            Vector3 target = currentlyHeld.transform.position + forceRatio * direction * targetLineLengthModifier;
+            Vector3[] positions = { currentlyHeld.transform.position, target };
+
+            _lineRenderer.startWidth = targetLineMaxWidth * forceRatio;
+            _lineRenderer.endWidth = targetLineMaxWidth * forceRatio;
+
+            _lineRenderer.SetPositions(positions);
+            _lineRenderer.enabled = true;
+        }
+
+    }
+
+    private void Throw()
+    {
+        if (!currentlyHeld)
+            return;
+
+        Vector3 mousePos = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            if (currentlyHeld == torch)
+                torch = null;
+            if (currentlyHeld == lantern)
+                lantern = null;
+            currentlyHeld.GetComponent<Rigidbody>().isKinematic = false;
+            guide.GetChild(0).parent = null;
+            Vector3 toMouse = hit.point - currentlyHeld.transform.position;
+            toMouse.x = 0; // Remove the useless depth component
+            currentlyHeld.GetComponent<Rigidbody>().AddForce(toMouse.normalized * _currentThrowForce);
+            currentlyHeld.GetComponent<Rigidbody>().AddTorque(Vector3.Cross(new Vector3(0, 1, 0), (toMouse.normalized * _currentThrowForce)));
+            currentlyHeld = null;
+        }
         _currentThrowForce = 0.0f;
         _lineRenderer.enabled = false;
-      }
     }
 
-    if (Input.GetMouseButtonUp(0))
+    private void ShootFireBall()
     {
-      if (currentlyHeld != scepter)
-      {
-        Throw();
-      }
-      else
-      {
-        ShootFireBall();
-      }
-    }
+        Vector3 mousePos = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        Rigidbody fireballClone = Instantiate(fireball, currentlyHeld.transform.position, currentlyHeld.transform.rotation).GetComponent<Rigidbody>();
 
-  }
-
-  void OnTriggerEnter(Collider col)
-  {
-    if (col.gameObject.tag == "torch")
-    {
-      if (!torch)
-        torch = col.gameObject;
+        // fireBall.velocity = transform.forward * speed;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            Vector3 toMouse = hit.point - currentlyHeld.transform.position;
+            toMouse.x = 0; // Remove the useless depth component
+            fireballClone.velocity = toMouse.normalized * fireBallBaseSpeed * Mathf.Clamp(_currentThrowForce / maxThrowForce, 0.2f , 1);
+        }
+        _currentThrowForce = 0.0f;
+        _lineRenderer.enabled = false;
     }
-    if (col.gameObject.tag == "lantern")
-    {
-      if (!lantern)
-        lantern = col.gameObject;
-    }
-    if (col.gameObject.tag == "scepter")
-    {
-      if (!scepter)
-        scepter = col.gameObject;
-    }
-  }
-
-  void OnTriggerStay(Collider col)
-  {
-    if (col.gameObject.tag == "torch")
-    {
-      if (!torch)
-        torch = col.gameObject;
-    }
-    if (col.gameObject.tag == "lantern")
-    {
-      if (!lantern)
-        lantern = col.gameObject;
-    }
-    if (col.gameObject.tag == "scepter")
-    {
-      if (!scepter)
-        scepter = col.gameObject;
-    }
-  }
-
-  void OnTriggerExit(Collider col)
-  {
-    if (col.gameObject.tag == "torch")
-    {
-      if (torch)
-        torch = null;
-    }
-    if (col.gameObject.tag == "lantern")
-    {
-      if (lantern)
-        lantern = null;
-    }
-    if (col.gameObject.tag == "scepter")
-    {
-      if (!scepter)
-        scepter = col.gameObject;
-    }
-  }
-
-  private void Pickup()
-  {
-    if (!(torch || lantern || scepter))
-      return;
-    if (torch)
-    {
-      torch.GetComponent<Rigidbody>().isKinematic = true;
-      torch.transform.SetParent(guide);
-      torch.transform.localRotation = guide.rotation;
-      torch.transform.Rotate(0, 0, 180); // Rotate otherwise it's upside-down...
-      torch.transform.position = guide.position;
-      currentlyHeld = torch;
-    }
-    else if (lantern)
-    {
-      lantern.GetComponent<Rigidbody>().isKinematic = true;
-      lantern.transform.SetParent(guide);
-      lantern.transform.localRotation = guide.rotation;
-      lantern.transform.Rotate(-90, 0, 0); // Rotate for 3D model
-      lantern.transform.position = guide.position;
-      currentlyHeld = lantern;
-    }
-    else if (scepter)
-    {
-      scepter.GetComponent<Rigidbody>().isKinematic = true;
-      scepter.transform.SetParent(guide);
-      scepter.transform.localRotation = guide.rotation;
-      scepter.transform.Rotate(-90, 0, 0); // Rotate for 3D model
-      scepter.transform.position = guide.position + new Vector3(0f, 1f, 0f);
-      currentlyHeld = scepter;
-    }
-  }
-
-  private void Drop()
-  {
-    if (!currentlyHeld)
-      return;
-    if (currentlyHeld == torch)
-    {
-      torch = null;
-    }
-    if (currentlyHeld == lantern)
-    {
-      lantern = null;
-    }
-    if (currentlyHeld == scepter)
-    {
-      scepter = null;
-    }
-    currentlyHeld.GetComponent<Rigidbody>().isKinematic = false;
-    guide.GetChild(0).parent = null;
-    currentlyHeld = null;
-    if (torch || lantern || scepter)
-    {
-      Pickup();
-    }
-  }
-
-  private void DrawThrowTarget()
-  {
-    if (!currentlyHeld)
-      return;
-
-    Vector3 mousePos = Input.mousePosition;
-    Ray castPoint = Camera.main.ScreenPointToRay(mousePos);
-    RaycastHit hit;
-    if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
-    {
-      Vector3 direction = (hit.point - currentlyHeld.transform.position).normalized;
-
-      float forceRatio = (_currentThrowForce / maxThrowForce);
-
-      Vector3 target = currentlyHeld.transform.position + forceRatio * direction * targetLineLengthModifier;
-      Vector3[] positions = { currentlyHeld.transform.position, target };
-
-      _lineRenderer.startWidth = targetLineMaxWidth * forceRatio;
-      _lineRenderer.endWidth = targetLineMaxWidth * forceRatio;
-
-      _lineRenderer.SetPositions(positions);
-      _lineRenderer.enabled = true;
-    }
-
-  }
-
-  private void Throw()
-  {
-    if (!currentlyHeld)
-      return;
-
-    Vector3 mousePos = Input.mousePosition;
-    Ray castPoint = Camera.main.ScreenPointToRay(mousePos);
-    RaycastHit hit;
-    if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
-    {
-      if (currentlyHeld == torch)
-        torch = null;
-      if (currentlyHeld == lantern)
-        lantern = null;
-      currentlyHeld.GetComponent<Rigidbody>().isKinematic = false;
-      guide.GetChild(0).parent = null;
-      Vector3 toMouse = hit.point - currentlyHeld.transform.position;
-      toMouse.x = 0; // Remove the useless depth component
-      currentlyHeld.GetComponent<Rigidbody>().AddForce(toMouse.normalized * _currentThrowForce);
-      currentlyHeld.GetComponent<Rigidbody>().AddTorque(Vector3.Cross(new Vector3(0, 1, 0), (toMouse.normalized * _currentThrowForce)));
-      currentlyHeld = null;
-    }
-    _currentThrowForce = 0.0f;
-    _lineRenderer.enabled = false;
-  }
-
-  private void ShootFireBall()
-  {
-    Vector3 mousePos = Input.mousePosition;
-    Ray castPoint = Camera.main.ScreenPointToRay(mousePos);
-    RaycastHit hit;
-    Transform fireBall = Instantiate(brick, transform.position, transform.rotation);
-    // fireBall.velocity = transform.forward * speed;
-    if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
-    {
-      Vector3 toMouse = hit.point - currentlyHeld.transform.position;
-      toMouse.x = 0; // Remove the useless depth component
-                     //currentlyHeld.GetComponent<Rigidbody>().AddForce(toMouse.normalized * _currentThrowForce);
-                     //currentlyHeld.GetComponent<Rigidbody>().AddTorque(Vector3.Cross(new Vector3(0, 1, 0), (toMouse.normalized * _currentThrowForce)));
-    }
-    _currentThrowForce = 0.0f;
-    _lineRenderer.enabled = false;
-  }
 }
