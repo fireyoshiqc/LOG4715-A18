38e1bd91c0e747e2ed8170df358bd2ec3cc1be3e
audrey <audrey.lebelr@gmail.com>
2018-11-06 13:44:06 -0500

    Merged gym 3-4 in project

---
+++ TP2/Gym1_Lancer/Assets/Scripts/BalancingPlatform.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BalancingPlatform : MonoBehaviour {
+
+    [SerializeField]
+    float mimimumAngle = -40;
+    [SerializeField]
+    float maximumAngle = 40;
+    [SerializeField]
+    float speed = 50f;
+
+	float rotationToApply = 0f;	
+    float currentRotation = 0f;
+    bool isClockwise, isMoving = false;
+
+    void Start () {
+    }
+
+    void Update()
+    {
+
+        if (isMoving)
+        {
+		    rotationToApply = (isClockwise)? - (speed * Time.deltaTime) : + (speed * Time.deltaTime); 
+            currentRotation += rotationToApply;
+
+            if (currentRotation > mimimumAngle && currentRotation  < maximumAngle)
+            {
+                transform.Rotate(rotationToApply, 0, 0);
+            } else
+            {
+                currentRotation -= rotationToApply;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        isMoving = true;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isClockwise = 
+            ((transform.position.z - collision.transform.position.z > 0) && (transform.position.y - collision.transform.position.y < 0));
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        isMoving = false;
+    }
+}
---
+++ TP2/Gym1_Lancer/Assets/Scripts/PulleyPlatform.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PulleyPlatform : MonoBehaviour {
+
+    [SerializeField]
+    bool isUp = true;
+    [SerializeField]
+    float speed = 5;
+
+    public bool isPositive = true;
+
+    float currentMovement = 0;
+    float length;
+
+    public Vector3 upmostPosition, downmostPosition;
+
+    [HideInInspector]
+    public float massToApply = 0;
+    [HideInInspector]
+    public float massOnPlatform;
+
+    void Start () {
+        length = Vector3.Distance(upmostPosition, downmostPosition);
+
+        //initialise the platform to a position
+        if (isUp) transform.position = upmostPosition;
+        else transform.position = downmostPosition;
+    }
+
+    void Update()
+    {
+        currentMovement = Time.deltaTime * speed * Mathf.Abs(massToApply) / length;
+
+        Move();
+
+        //massOnPlatform = 0;
+    }
+
+    void Move()
+    {
+        if (goingUp() && transform.position != upmostPosition)
+        {
+            MoveUp();
+        } else if (transform.position != downmostPosition)
+        {
+            MoveDown();
+        }
+    }
+
+    bool goingUp()
+    {
+        if ((isPositive && massToApply < 0) || (!isPositive && massToApply > 0))
+        {
+            return true;
+        }
+            
+        return false;
+    }
+
+    void MoveUp()
+    {
+        transform.position = Vector3.Lerp(transform.position, upmostPosition, currentMovement); 
+    }
+
+    void MoveDown()
+    {
+        transform.position = Vector3.Lerp(transform.position, downmostPosition, currentMovement);   
+    }
+        
+    void OnCollisionEnter(Collision collision)
+    {
+        isUp = (transform.position.y - collision.transform.position.y > 0);
+        massOnPlatform = collision.rigidbody.mass;
+        //massOnPlatform = (isUp) ? -collision.rigidbody.mass : collision.rigidbody.mass;
+    }
+
+     private void OnCollisionStay(Collision collision)
+    {
+        //isUp = (transform.position.y - collision.transform.position.y > 0);
+        //massOnPlatform += collision.rigidbody.mass;
+        //massOnPlatform += (isUp) ? collision.rigidbody.mass : -collision.rigidbody.mass;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        massOnPlatform = 0;
+    }
+}
+
