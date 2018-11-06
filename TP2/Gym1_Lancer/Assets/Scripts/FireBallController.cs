0d6e0345384abd8ee5688ec24032b17432857281
Fudgyking <escalado19@hotmail.com>
2018-11-05 21:56:54 -0500

    Fix de nom de gym et ajout du FireBallController oublié...

---
+++ TP2/Gym1_Lancer/Assets/Scripts/FireBallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallController : MonoBehaviour {
+
+    public LayerMask layer;
+    public int maxTravelDistance = 50;
+    private Vector3 startPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(startPosition, transform.position) >= maxTravelDistance)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        Destroy(gameObject);
+    }
+}
