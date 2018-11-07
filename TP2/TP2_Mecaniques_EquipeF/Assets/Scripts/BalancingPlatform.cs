eaae9aa24a8caabb123ab36c347baa3f5d8af170
audrey <audrey.lebelr@gmail.com>
2018-11-06 21:46:06 -0500

    Cleanup and added missing ranges

--- TP2/TP2_Mecaniques_EquipeF/Assets/Scripts/BalancingPlatform.cs
+++ TP2/TP2_Mecaniques_EquipeF/Assets/Scripts/BalancingPlatform.cs
@@ -1,61 +1,77 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class BalancingPlatform : MonoBehaviour {
 
     [SerializeField]
+    [Range(-80, 0)]
     float mimimumAngle = -40;
     [SerializeField]
+    [Range(0, 80)]
     float maximumAngle = 40;
     [SerializeField]
+    [Range(0.1f, 10)]
     float speed = 1f;
 
     float length = 0;
-    float lowestPoint = 0;
-    bool isClockwise, isUnder, isMoving = false;
+    bool isClockwise, isOver, isMoving = false;
     Quaternion minAngleQuat, maxAngleQuat;
+
+    float timeOfLastMove;
 
     void Start () {
         minAngleQuat = Quaternion.AngleAxis(mimimumAngle, Vector3.left);
         maxAngleQuat = Quaternion.AngleAxis(maximumAngle, Vector3.left);
 
         length = Quaternion.Angle(minAngleQuat, maxAngleQuat);
+
+        timeOfLastMove = Time.time;
     }
 
     void Update()
     {
+        if (timeOfLastMove - Time.time > 0.2)
+            isMoving = false;
+
+        
         if (isMoving)
         {
             float t = Time.deltaTime * speed * 10 / length;
 
             if (isClockwise)
-                transform.rotation = Quaternion.Slerp(transform.rotation, maxAngleQuat, t); 
+            {
+                float remainingDistance =  Quaternion.Angle(minAngleQuat, maxAngleQuat) * speed / length;
+                transform.rotation = Quaternion.Slerp(transform.rotation, maxAngleQuat, t);
+            }
             else
-                transform.rotation = Quaternion.Slerp(transform.rotation, minAngleQuat, t); 
+            {
+                float remainingDistance =  Quaternion.Angle(minAngleQuat, maxAngleQuat) * speed / length;
+                transform.rotation = Quaternion.Slerp(transform.rotation, minAngleQuat, t);
+            }
+                 
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
         isMoving = true;
-        isUnder = (collision.collider.GetType() == typeof(SphereCollider));
+        isOver = (collision.collider.GetType() == typeof(SphereCollider));
+        timeOfLastMove = Time.time;
     }
 
     private void OnCollisionStay(Collision collision)
     {
         if (transform.position.z - collision.transform.position.z > 0)
         {
-            isClockwise = (isUnder) ? true : false;
+            isClockwise = (isOver) ? true : false;
         }
         else
         {
-            isClockwise = (isUnder) ? false : true;
+            isClockwise = (isOver) ? false : true;
         }
+        timeOfLastMove = Time.time;
     }
 
     void OnCollisionExit(Collision col)
     {
-        isMoving = false;
     }
 }
