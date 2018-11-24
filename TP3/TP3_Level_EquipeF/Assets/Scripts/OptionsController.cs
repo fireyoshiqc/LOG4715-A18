82382661f1718071663036613692049dccf61ca9
Félix Boulet <fire_yoshi_qc@hotmail.com>
2018-11-24 17:19:48 -0500

    Ajouté main menu fonctionnel

---
+++ TP3/TP3_Level_EquipeF/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsController : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public TextMeshProUGUI volumeText;
+
+	// Use this for initialization
+	void Start () {
+        volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(); });
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void ChangeVolume()
+    {
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
+        AudioListener.volume = PlayerPrefs.GetFloat("volume");
+        volumeText.SetText(((int)(volumeSlider.value * 100)).ToString());
+    }
+}
