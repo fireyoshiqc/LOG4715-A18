f9efbe7e8dbe088bd5fa6ec9e3cd904b7d545892
Félix Boulet <fire_yoshi_qc@hotmail.com>
2018-11-25 23:45:19 -0500

    Avancement

--- TP3/TP3_Level_EquipeF/Assets/Scripts/CameraController.cs
+++ TP3/TP3_Level_EquipeF/Assets/Scripts/CameraController.cs
@@ -15,6 +15,8 @@ public class CameraController : MonoBehaviour {
     Vector3 lastTargetPosition;
     Vector3 currentVelocity;
     Vector3 lookAheadPos;
+    Vector3 newPos;
+    Vector3 gfxVelocity;
 
     // Use this for initialization
     void Start()
@@ -25,7 +27,7 @@ public class CameraController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // only update lookahead pos if accelerating or changed direction
         float xMoveDelta = (target.position - lastTargetPosition).x;
@@ -38,11 +40,11 @@ public class CameraController : MonoBehaviour {
         }
         else
         {
-            lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+            lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.fixedDeltaTime * lookAheadReturnSpeed);
         }
 
         Vector3 aheadTargetPos = target.position + Vector3.up * offsetY + lookAheadPos + Vector3.forward * offsetZ;
-        Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+        newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
 
         transform.position = newPos;
 
